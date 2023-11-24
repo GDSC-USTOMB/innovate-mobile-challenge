29bf858fea4d1b48ebd9246e324f4ffafc56c3fb
Mr.UNIX <mrunix00@protonmail.com>
2023-11-24 11:03:44 +0100

    set bottom appbar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'core/go_router.dart';
 import 'theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(systemNavigationBarColor: Color(0xff252525)),
+  );
   runApp(
     MaterialApp.router(
       title: 'Notes',
